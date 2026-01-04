8a08ac361f932e6b679b770d852c9504401e6e06
dainius1984 <marcinarturchmielnicki@gmail.com>
2026-01-04 11:43:47 +0100

    wokring widget

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import About from "./pages/About";
@@ -14,20 +14,19 @@ import Nawadnianie from "./pages/services/Nawadnianie";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout><Home /></Layout>} />
-        <Route path="/o-nas" element={<Layout><About /></Layout>} />
-        <Route path="/portfolio" element={<Layout><Portfolio /></Layout>} />
-        <Route path="/portfolio/tarasy" element={<Layout><Tarasy /></Layout>} />
-        <Route path="/portfolio/ogrody" element={<Layout><Ogrody /></Layout>} />
-        <Route path="/portfolio/przed-po" element={<Layout><PrzedPo /></Layout>} />
-        <Route path="/uslugi" element={<Layout><Services /></Layout>} />
-        <Route path="/uslugi/projektowanie-ogrodow" element={<Layout><ProjektowanieOgrodow /></Layout>} />
-        <Route path="/uslugi/budowa-tarasow" element={<Layout><BudowaTarasow /></Layout>} />
-        <Route path="/uslugi/nawadnianie" element={<Layout><Nawadnianie /></Layout>} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout><Home /></Layout>} />
+      <Route path="/o-nas" element={<Layout><About /></Layout>} />
+      <Route path="/portfolio" element={<Layout><Portfolio /></Layout>} />
+      <Route path="/portfolio/tarasy" element={<Layout><Tarasy /></Layout>} />
+      <Route path="/portfolio/ogrody" element={<Layout><Ogrody /></Layout>} />
+      <Route path="/portfolio/przed-po" element={<Layout><PrzedPo /></Layout>} />
+      <Route path="/uslugi" element={<Layout><Services /></Layout>} />
+      <Route path="/uslugi/projektowanie-ogrodow" element={<Layout><ProjektowanieOgrodow /></Layout>} />
+      <Route path="/uslugi/budowa-tarasow" element={<Layout><BudowaTarasow /></Layout>} />
+      <Route path="/uslugi/nawadnianie" element={<Layout><Nawadnianie /></Layout>} />
+      <Route path="*" element={<Layout><Home /></Layout>} />
+    </Routes>
   );
 }
 
